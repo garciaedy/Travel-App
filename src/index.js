52e12e8e836146dbfdde47919a9f495e2da326f7
Edy A. Garcia Acevedo <edygarcia@macbook-pro.attlocal.net>
2020-06-05 23:16:18 -0400

    added helmet

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
 
 
 const app = express();
 app.use(morgan('common'));
+app.use(helmet());
+
+
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
