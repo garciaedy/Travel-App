05a647c7d58d7fcc9f2de4fca91869081d7d3b6d
Edy A. Garcia Acevedo <edygarcia@macbook-pro.attlocal.net>
2020-06-07 13:12:38 -0400

    up to date

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 
+
+
+const middlewears = require('./middlewares');
+
+
 const app = express();
 app.use(morgan('common'));
 app.use(helmet());
@@ -16,20 +21,9 @@ app.get('/', (req, res) => {
     });
 });
 
-app.use((req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-});
+app.use(middlewears.notFound);
 
-app.use((error, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-     res.status(statusCode);
-    res.json({
-        message: error.message,
-        stack: process.env.NODE_ENV === 'production' ? '' : error.stack,
-    }); 
-});
+app.use(middlewears.errorHandler);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
