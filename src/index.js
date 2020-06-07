1dad69444b2ffd8a403462b6aed4587369f8224a
Edy A. Garcia Acevedo <edygarcia@macbook-pro.attlocal.net>
2020-06-06 23:11:20 -0400

    created error

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 
-
 const app = express();
 app.use(morgan('common'));
 app.use(helmet());
@@ -17,7 +16,20 @@ app.get('/', (req, res) => {
     });
 });
 
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
 
+app.use((error, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);
+    res.json({
+        message: error.message,
+        stack: error.stack,
+    }); 
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
