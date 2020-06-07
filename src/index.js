bc3df57a089326b6750e82a28c35d0e434245c20
Edy A. Garcia Acevedo <edygarcia@macbook-pro.attlocal.net>
2020-06-06 23:17:47 -0400

    error handler middlewear

--- src/index.js
+++ src/index.js
@@ -24,10 +24,10 @@ app.use((req, res, next) => {
 
 app.use((error, req, res, next) => {
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    res.status(statusCode);
+     res.status(statusCode);
     res.json({
         message: error.message,
-        stack: error.stack,
+        stack: process.env.NODE_ENV === 'production' ? '' : error.stack,
     }); 
 });
 
