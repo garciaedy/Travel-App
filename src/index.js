459da11bed9ee6a00f1a540798d86605d3d381b0
garciaedy <edygarcia97@gmail.com>
2021-08-26 23:20:31 -0400

    updaded middleware

--- src/index.js
+++ src/index.js
@@ -1,32 +1,24 @@
 const express = require('express');
 const morgan = require('morgan');
-const helmet = require('helmet');
+const helmet = require('helmet')
 const cors = require('cors');
-
-
-
-const middlewears = require('./middlewares');
 
 
 const app = express();
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3000'
 }));
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) =>{
     res.json({
-        message: 'Hello World',
+        message:'Hello world'
     });
 });
 
-app.use(middlewears.notFound);
-
-app.use(middlewears.errorHandler);
-
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-    console.log(`Listening at http://localhost:3001: ${port}`);
+const port = process.env.PORT || 1337;
+app.listen(port, () =>{
+    console.log(`Listening at http://localhost:${port}`);
 
 });
