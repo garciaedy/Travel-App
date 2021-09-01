ea4cde12bc5fb6074790ce5dc93027bc42acafde
garciaedy <edygarcia97@gmail.com>
2021-08-31 21:50:55 -0400

    completed adding rest of the model

--- src/models/LogEntry.js
+++ src/models/LogEntry.js
@@ -1,28 +1,46 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
+
 const { Schema } = mongoose;
 
-const requiredString = {
-  type: String,
+const requiredNumber = {
+  type: Number,
   required: true,
 };
 
-const logEntrySchema = new Schema({
-  title: requiredString,
-  description: String,
-  comments: String,
-  rating: {
+const logEntrySchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    comments: String,
+    image: String,
+    rating: {
       type: Number,
       min: 0,
-      max:10,
+      max: 10,
       default: 0,
-  }
-
-
-  comments: [{ body: String, date: Date }],
-  date: { type: Date, default: Date.now },
-  hidden: Boolean,
-  meta: {
-    votes: Number,
-    favs: Number,
+    },
+    latitude: {
+      ...requiredNumber,
+      min: -90,
+      max: 90,
+    },
+    longitude: {
+      ...requiredNumber,
+      min: -180,
+      max: 180,
+    },
+    visitDate: {
+      required: true,
+      type: Date,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
+
+const LogEntry = mongoose.model("LogEntry", logEntrySchema);
+module.exports = LogEntry;
