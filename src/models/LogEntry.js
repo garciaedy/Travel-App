d089974e703cfbfd67cd622e5fd2eb78d02ba588
garciaedy <edygarcia97@gmail.com>
2021-08-31 21:22:23 -0400

    added model

--- src/models/LogEntry.js
+++ src/models/LogEntry.js
@@ -1,37 +1,28 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+import mongoose from "mongoose";
+const { Schema } = mongoose;
 
-const requiredNumber= {
-    type: Number,
-    requires: true,
+const requiredString = {
+  type: String,
+  required: true,
 };
 
-const defaultRequiredDate ={
-    type: Date,
-    default: Date.now,
-    required: true,
+const logEntrySchema = new Schema({
+  title: requiredString,
+  description: String,
+  comments: String,
+  rating: {
+      type: Number,
+      min: 0,
+      max:10,
+      default: 0,
+  }
 
-}
 
-conts LogEntrySchema = new Schema({
-
- title: {
-    type: String,
-    required: true,
-},
- description: String,
- comments: String,
- image: String,
- rating: {
-     type: Number,
-     min: 0,
-     max: 10,
-     default: 0
- },
- latitude: requiredNumber,
- longitude: requiredNumber,
-
- created_at: defaultRequiredDate,
- updated_at: defaultRequiredDate,
-
+  comments: [{ body: String, date: Date }],
+  date: { type: Date, default: Date.now },
+  hidden: Boolean,
+  meta: {
+    votes: Number,
+    favs: Number,
+  },
 });
